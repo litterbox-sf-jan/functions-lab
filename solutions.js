139f0c5691ee8bc98df608b01e6b55f7ea5c36e6
Peter Schulz <prschulz88@gmail.com>
2015-01-14 15:23:22 -0800

    Bonus solutions

--- solutions.js
+++ solutions.js
@@ -43,3 +43,32 @@ product *= arr[i];
 }
 return product;
 }
+
+var reverseString = function (str){
+  var arr = str.split('');
+  var revArr = [];
+  for (var i = arr.length - 1; i >= 0; i--) {
+    revArr.push(arr[i]);
+  }
+  return revArr.join('');
+};
+
+function findLongestWord (arr) {
+  longest = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > longest) {
+      longest = arr[i].length;
+    }
+  }
+  return longest;
+}
+
+function filterLongWords (arr,num) {
+  newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].length > num){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
